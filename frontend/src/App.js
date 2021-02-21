53fa2b51674f981bc468561d643abff84d15c399
Chloe Adcock <adcockchloe@gmail.com>
2021-02-21 14:06:48 +0000

    Created map container using google maps api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/navbar/Navbar';
+import MapContainer from './components/mapContainer/MapContainer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <MapContainer />
     </div>
   );
 }
