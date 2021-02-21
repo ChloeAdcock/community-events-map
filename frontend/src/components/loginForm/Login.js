8e11b63b8e30d4ed4a76a24feb90699a4bcc2316
Chloe Adcock <adcockchloe@gmail.com>
2021-02-21 16:03:43 +0000

    login form

--- frontend/src/components/loginForm/Login.js
+++ frontend/src/components/loginForm/Login.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Textfield from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 function Login() {
     return (
         <div>
-            <Textfield label='Username' variant='outlined' />
-            <Textfield label='Password' variant='outlined' />
+            <Typography varient='h1'>Login</Typography>
+            <Textfield label='Username' variant='outlined' required />
+            <Textfield label='Password' variant='outlined' required />
+            <Button>Submit</Button>
         </div>
     )
 }
