3c963483787861b1773763d10e5b05c7f0b49e17
Chloe Adcock <adcockchloe@gmail.com>
2021-02-23 17:06:59 +0000

    login form

--- frontend/src/components/loginForm/LoginForm.js
+++ frontend/src/components/loginForm/LoginForm.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Textfield from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from '../../redux/actions/accounts/accounts';
 
 function LoginForm() {
+
+    const dispatch = useDispatch();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(login({
+            username: username,
+            password: password
+        }))
+    }
+
     return (
         <div>
-            <Typography varient='h1'>Login</Typography>
-            <Textfield label='Username' variant='outlined' required />
-            <Textfield label='Password' variant='outlined' required />
-            <Button>Submit</Button>
+            <Typography 
+            varient='h1'>
+                Login
+            </Typography>
+            <Textfield
+                onChange={e => setUsername(e.target.value)}
+                label='Username'
+                variant='outlined'
+                required />
+            <Textfield
+                onChange={e => setPassword(e.target.value)}
+                label='Password'
+                variant='outlined'
+                required />
+            <Button
+                onClick={handleSubmit}>
+                Submit
+                </Button>
         </div>
     )
 }
--- frontend/src/redux/actions/accounts/accounts.js
+++ frontend/src/redux/actions/accounts/accounts.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { LOGGED_IN, LOGIN_ERROR } from '../types';
 
-export const login = () => (dispatch) => {
+export const login = (data) => (dispatch) => {
     return axios
         .post('http://127.0.0.1:8000/accounts/login', data)
         .then(res => {
