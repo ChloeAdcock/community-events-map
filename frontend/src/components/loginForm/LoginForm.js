7ef1cad313a5f0426ae6ddaa9896ad81a31f3fd0
Chloe Adcock <adcockchloe@gmail.com>
2021-02-23 17:22:29 +0000

    Error handling for login

--- frontend/src/components/loginForm/LoginForm.js
+++ frontend/src/components/loginForm/LoginForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Textfield from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { Redirect } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { login } from '../../redux/actions/accounts/accounts';
 
@@ -10,6 +11,8 @@ function LoginForm() {
     const dispatch = useDispatch();
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const errorState = useSelector(state => state.accounts.error);
+    const user = useSelector(state => state.accounts.currentUser);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -19,29 +22,34 @@ function LoginForm() {
         }))
     }
 
-    return (
-        <div>
-            <Typography 
-            varient='h1'>
-                Login
-            </Typography>
-            <Textfield
-                onChange={e => setUsername(e.target.value)}
-                label='Username'
-                variant='outlined'
-                required />
-            <Textfield
-                onChange={e => setPassword(e.target.value)}
-                type='password'
-                label='Password'
-                variant='outlined'
-                required />
-            <Button
-                onClick={handleSubmit}>
-                Submit
-                </Button>
-        </div>
-    )
+    if (!errorState && user) {
+        return <Redirect to='/map' />
+    } else {
+        return (
+            <div>
+                <Typography
+                    varient='h1'>
+                    Login
+                </Typography>
+                <Textfield
+                    onChange={e => setUsername(e.target.value)}
+                    label='Username'
+                    variant='outlined'
+                    required />
+                <Textfield
+                    onChange={e => setPassword(e.target.value)}
+                    type='password'
+                    label='Password'
+                    variant='outlined'
+                    required />
+                {errorState ? <Typography>Incorrect username or password</Typography> : <div />}
+                <Button
+                    onClick={handleSubmit}>
+                    Submit
+                    </Button>
+            </div>
+        )
+    }
 }
 
 export default LoginForm;
